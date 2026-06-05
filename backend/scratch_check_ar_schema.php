ba2fb5e9e762e6118a6cfa3942d3e175b721058d
jeffnst <jeffryan_it@yahoo.co.id>
2026-06-05 14:15:55 +0700

    chore: remove unused test configuration and associated utility files

--- backend/scratch_check_ar_schema.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "dev", "12345678", "raporsmpit");
-$res = $mysqli->query("SHOW CREATE TABLE anggota_rombel");
-$row = $res->fetch_assoc();
-print_r($row);
--- backend/scratch_check_rombel.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "dev", "12345678", "raporsmpit");
-$res = $mysqli->query("SELECT id, nama_rombel FROM rombel WHERE id IN (18, 19)");
-while($row = $res->fetch_assoc()) {
-    print_r($row);
-}
